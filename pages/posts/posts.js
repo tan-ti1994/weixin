97ebc1e1f23b15a65452626e5e7e6441e468a546
tan-ti1994 <TCT2016Y@163.com>
2020-01-16 16:17:49 +0800

    轮播图完成

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -1,94 +1,66 @@
-//index.js
+// pages/posts/posts.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    navr:[
-    {id:1 ,name:'项目'},
-    {id:2 ,name:'文件'},
-    {id:3 ,name:'编辑'},
-    {id:4 ,name:'工具'}
-    ],
-    // 轮播图
-    swiper_list:[],
-    dh_list:[]
+
   },
-  
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.request({
-      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
-      success: (res) => {
-        this.setData({
-          swiper_list: res.data.message
-        })
-      }
-    }),
-    wx.request({
-      url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
-      success:(res)=>{
-        this.setData({
-          dh_list:res.data.message
-        })
-      }
-    })
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
-
-
-  
 })
